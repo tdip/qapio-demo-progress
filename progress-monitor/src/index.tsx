e5fc356e08616d2ab4a4c84795b38c6c22feaa04
Ernesto Rodriguez <ernesto.rodriguez@quantifio.no>
2020-11-27 11:15:01 -0600

    Initial commit

---
+++ progress-monitor/src/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+import * as ReactDom from "react-dom"
+
+function MainComponent(props: {}) {
+
+    return (
+        <div>
+            Hello!
+        </div>
+    );
+}
+
+const start = async (event) => {
+
+    ReactDom.render(
+        <MainComponent />,
+        document.getElementById("root")
+    );
+}
+
+window.addEventListener('load', start);
