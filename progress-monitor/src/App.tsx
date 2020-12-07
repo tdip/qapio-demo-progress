c8ab64f22d7f554bac00d119f91f60ad74208276
JorgeMarroquin <jorge.marroquin.ochoa@gmail.com>
2020-12-07 13:20:17 -0600

    datePikcer test

--- progress-monitor/src/App.tsx
+++ progress-monitor/src/App.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 
-import { Classes} from "@blueprintjs/core";
-
-import { getStyles } from "./styles/styles_Graphics";
 import {VictoryLine, VictoryBar, VictoryChart, VictoryZoomContainer, VictoryAxis,} from "victory";
 import ReactLoading from "react-loading";
 import { DateRange, DateRangePicker } from "@blueprintjs/datetime";
-//import * as fa from "react-icons/fa";
+import { Classes} from "@blueprintjs/core";
 
+import { getStyles } from "./styles/styles_Graphics";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/styles.scss";
 
@@ -18,19 +16,14 @@ const org = "qapio";
 const bucket = "qapio";
 const client = new InfluxDB({ url: "http://localhost:8086", token: token });
 
-interface tabla {
-  x: Date;
-  y: number;
-}
-type ReactForm = React.FormEvent<HTMLFormElement>;
+interface tabla {x: Date; y: number;}
 type ReactFormInput = React.FormEvent<HTMLInputElement>;
 
 function App() {
-  var temp: tabla[] = [];
 
   const [date, setDate] = React.useState({ init: "", end: "" });
   const [dateRange, setDateRange] = React.useState([null, null]);
-  const [arreglo, setArreglo] = React.useState<tabla[]>(temp);
+  const [arreglo, setArreglo] = React.useState<tabla[]>([]);
   const [styles, setStyles] = React.useState(getStyles());
   const [msg, setMsg] = React.useState(
     <div className="loading">
@@ -54,7 +47,7 @@ function App() {
       |> range(start: ${dateToString(new Date(dateRange[0]), true)}T00:00:00Z, stop: ${dateToString(new Date(dateRange[1]), true)}T23:59:00Z)
       |> filter(fn: (r) => r._measurement == "go_gc_duration_seconds")
       |> filter(fn: (r) => r._field == "count")
-      |> aggregateWindow(fn: mean, every: 12h)
+      |> aggregateWindow(fn: mean, every: 24h)
       `;
 
     queryApi.queryRows(query, {
@@ -93,12 +86,17 @@ function App() {
   function dateToString(date: Date, option = false){
     var year = (date.getFullYear()).toString();
     var month = (date.getMonth() + 1).toString();
-    var day = (date.getDate()).toString();
+    if(date.getDate() <= 9){
+      var day = "0" + (date.getDate()).toString();
+    }else{
+      var day = (date.getDate()).toString();
+    }
+    
 
     if(option){
-      return year + "-" + month + "-" + day;
+      return (year + "-" + month + "-" + day).toString();
     }else{
-      return day + "/" + month + "/" + year;
+      return (day + "/" + month + "/" + year).toString();
     }
   }
 
@@ -117,28 +115,8 @@ function App() {
         className="datePiker"
         onChange={handleDateChange}
 
-/></div>
-            <label>
-              From
-              <br />
-              <input
-                name="init"
-                type="date"
-                value={date.init}
-                onChange={handleInputChange}
-              />
-            </label>
-
-            <label>
-              To
-              <br />
-              <input
-                name="end"
-                type="date"
-                value={date.end}
-                onChange={handleInputChange}
-              />
-            </label>
+          /></div>
+           
             <br />
             <button id="submit" onClick={sendData} type="submit">
               Search
